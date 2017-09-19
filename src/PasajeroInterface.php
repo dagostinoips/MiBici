eda1f1402cc540278c86380eb247e5b2b4c790ff
CDTux <guybaphomet@gmail.com>
2017-09-19 17:53:00 -0300

    Actualizada la interfase Pasajero para el ticket #16.

--- src/PasajeroInterface.php
+++ src/PasajeroInterface.php
@@ -4,6 +4,24 @@ namespace Mibici;
 
 interface PasajeroInterface {
 
-    public function dni();
+	public $tiposdeplan;
+
+	protected $dni, $saldo, $plan, $viajesdisponibles;
+
+    public function getDNI();
+
+    public function getSaldo();
+
+    public function getPlan();
+
+    public function getViajesDisponibles();
+
+    protected function agregarSaldo();
+
+    protected function retirarSaldo();
+
+    protected function comprarPlan();
+
+    protected function usarViaje(ViajeInterface $viaje);
 
 }
