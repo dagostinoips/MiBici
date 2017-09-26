31fdb6306e702e32c152fd97344ab408e654254a
AleDeCicco <31454517+AleDeCicco@users.noreply.github.com>
2017-09-26 17:47:46 -0300

    Eliminacion de 2 asserciones por error conceptual

--- tests/BicicletaTest.php
+++ tests/BicicletaTest.php
@@ -9,9 +9,7 @@ class BicicletaTest extends TestCase {
     public function testEstacionEnServicio() {
 
         $bici = new Bicicleta;
-        
-        $this-> assertEquals($bici->tiempoEnUso(),$this->tiempo,"La funcion no devuelve el tiempo en uso guardado en la variable tiempo");
-        $this-> assertEquals($bici->estado(),$this->status,"La funcion no devuelve el estado de la bicicleta guardado en la variable status");
+    
         $this->assertInternalType( "bool", $this->estado() );
     }
 }
