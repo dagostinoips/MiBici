5777fa14ccbaeb6b405b4f62c6a9aee40afa651f
pancreasio <32310082+pancreasio@users.noreply.github.com>
2017-09-26 17:48:27 -0300

    Update EstacionTest.php

--- tests/EstacionTest.php
+++ tests/EstacionTest.php
@@ -1,20 +1,14 @@
 <?php
-
 namespace Mibici;
-
 use PHPUnit\Framework\TestCase;
-
 class EstacionTest extends TestCase {
-
     public function testEstacionEnServicio() {
         // Las estaciones nuevas siempre están en servicio.
         // Primero en el construct es anclajes disponibles y el segundo es anclajes totales
-        $anclajesdisp = 10;
-        $anclajestota = 18;
-        $estacion = new Estacion($anclajesdisp,$anclajestota);
-        $this->assertEquals($estacion->anclajesDisponibles(),$anclajesdisp);
-        $this->assertEquals($estacion->anclajesTotales(),$anclajestota);
+        $anclajes = 18;
+        $estacion = new Estacion($anclajes);
+        $this->assertEquals($estacion->anclajesDisponibles(),$anclajes);
+        $this->assertEquals($estacion->anclajesTotales(),$anclajes);
         $this->assertTrue($estacion->enServicio());
     }
-
 }
