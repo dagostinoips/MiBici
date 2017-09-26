a81b3424316c4da3c0a1640ffb6b5e040bd571f0
sofiachami <sofiachami99@gmail.com>
2017-09-26 16:43:02 -0300

    Commit 1, Issue #1
    Agregado un constructor con dos parametros, especificadas las funciones anclajesDisponibles, dejarBici, sacarBici. Agregadas las funciones activarEstacion y desactivarEstacion.

--- src/Estacion.php
+++ src/Estacion.php
@@ -3,29 +3,53 @@
 namespace Mibici;
 
 class Estacion implements EstacionInterface {
-
+    
+    protected $estado = True;
+    protected $anclajes = 15;
+    protected $aDisponible;
+    protected $bicis;
+    
+    public function __construct($e, $b) {
+        $this->estado = $e;
+        $this->bicis = $b;
+    }    
+    
     public function sacarBici(BicicletaInterface $bici) {
-
+        if ($this->bicis > 0) {
+            $this->aDisponible = $this->aDisponible + 1;
+            $this->bicis = $this->bicis - 1;
+        }
+        else {
+            return "no se puede sacar la bici";
+        }
     }
 
     public function dejarBici(BicicletaInterface $bici) {
-
+        if ($this->aDisponible > 0) {
+            $this->aDisponible = $this->aDisponible - 1;
+            $this->bicis = $this->bicis + 1;
+        }
+        else {
+            return "no se puede dejar la bici";
+        }
     }
 
     public function anclajesDisponibles() {
-
+        $this->aDisponible = $this->anclajes - $this->bicis;     
     }
-
-    public function anclajesTotales() {
-
-    }
-
-    public function bicicletasDisponibles() {
-
-    }
-
+    
     public function enServicio() {
-        return TRUE;
+        
+        return $this->estado;
+        
+       
     }
-
+    public function desactivarEstacion() { 
+        $this->estado = False;
+ 
+    }
+    
+    public function activarEstacion() {
+        $this->estado = True;
+    }    
 }
