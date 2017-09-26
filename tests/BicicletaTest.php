85204c4db2ed3e50aab334c5b757c2d435149f66
AleDeCicco <31454517+AleDeCicco@users.noreply.github.com>
2017-09-26 16:09:42 -0300

    mensaje de error updated

--- tests/BicicletaTest.php
+++ tests/BicicletaTest.php
@@ -4,14 +4,14 @@ namespace Mibici;
 
 use PHPUnit\Framework\TestCase;
 
-class BicicletaTest extends TestCase,Bicicleta {
+class BicicletaTest extends TestCase {
     
     public function testEstacionEnServicio() {
 
         $bici = new Bicicleta;
         
-        $this-> assertEquals($bici->tiempoEnUso(),$this->tiempo,"La funcion no devuelve el tiempo esperado");
-        $this-> assertEquals($bici->estado(),$this->status,"La funcion no devuelve el estado esperado");
-        $this->assertInternalType("bool", $this->status);
+        $this-> assertEquals($bici->tiempoEnUso(),$this->tiempo,"La funcion no devuelve el tiempo en uso guardado en la variable tiempo");
+        $this-> assertEquals($bici->estado(),$this->status,"La funcion no devuelve el estado de la bicicleta guardado en la variable status");
+        $this->assertInternalType( "bool", $this->estado() );
     }
 }
